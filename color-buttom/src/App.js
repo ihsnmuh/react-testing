626151b56533888966cb552fd13997cd5d256da3
Muhammad Ihsan <ihsan@femaledaily.com>
2023-03-20 14:03:53 +0700

    feat: testing button and checkbox click

--- color-buttom/src/App.js
+++ color-buttom/src/App.js
@@ -4,16 +4,27 @@ import './App.css';
 function App() {
 
   const [color, setColor] = useState("red")
+  const [disableChecked, setDisableChecked] = useState(false)
   const newColor = color === "red" ? "blue" : "red"
 
 
   return (
-    <button 
-      onClick={() => setColor(newColor)} 
-      style={{backgroundColor: color}}
-      >
-      {`Change color to ${newColor}`}
-    </button>
+    <>
+      <button 
+        onClick={() => setColor(newColor)} 
+        style={{backgroundColor: color}}
+        disabled={disableChecked}
+        >
+        {`Change color to ${newColor}`}
+      </button>
+      <input 
+        type="checkbox"
+        id="enable-button-checkbox"
+        onChange={e => 
+          setDisableChecked(e.target.checked)
+        }
+      />
+    </>
   );
 }
 
--- color-buttom/src/App.test.js
+++ color-buttom/src/App.test.js
@@ -16,7 +16,40 @@ test('Button has correct initial color, and change the text', () => {
   // expect the color background to be blue
   expect(colorButton).toHaveStyle({"background-color": "blue"})
 
-  // expect the text to be Change color to red
+  // expect the text to be 'Change color to red'
   expect(colorButton).toHaveTextContent('Change color to red')
 
 });
+
+test("initial condition", () => {
+  render(<App/>)
+
+  // check the button starts out enable
+  const colorButton = screen.getByRole("button", {name: "Change color to blue"})
+  expect(colorButton).toBeEnabled()
+
+  // check the checkbox starts out unchecked
+  const checkBox = screen.getByRole("checkbox")
+  expect(checkBox).not.toBeChecked()
+})
+
+test("Checkbox disables botton on first click and enable on second click", () => {
+  render(<App/>)
+
+  // check the button starts out enable
+  const colorButton = screen.getByRole("button", {name: "Change color to blue"})
+  expect(colorButton).toBeEnabled()
+
+  // check the checkbox starts out unchecked
+  const checkBox = screen.getByRole("checkbox")
+  expect(checkBox).not.toBeChecked()
+
+  fireEvent.click(checkBox)
+
+  // check the checkbox starts out checked
+  expect(checkBox).toBeChecked()
+
+  // check the checkbox start out disable
+  expect(colorButton).toBeDisabled()
+
+})
